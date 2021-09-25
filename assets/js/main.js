3d72d12b13973043e07ee97370922860003d52bd
devferx <ferkki@hotmail.com>
2021-09-25 18:09:14 -0400

    :sparkles: [ADD] Interactive filter and rotation

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const $slider = document.getElementById("slider");
+const $filter = document.getElementById("filter");
+const $sunIcon = document.getElementById("sun");
+
+$slider.addEventListener("input", (ev) => {
+  const value = ev.target.value / 100;
+
+  $filter.style.opacity = 1 - value - 0.1;
+  $sunIcon.style.transform = `rotate(${360 - 360 * value}deg)`;
+});
