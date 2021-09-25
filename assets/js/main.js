034cc1f3add052a80af1bee1092fdf22d19bdba5
devferx <ferkki@hotmail.com>
2021-09-25 18:35:36 -0400

    :lipstick: [ADD] Styles to slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,5 +6,5 @@ $slider.addEventListener("input", (ev) => {
   const value = ev.target.value / 100;
 
   $filter.style.opacity = 1 - value - 0.1;
-  $sunIcon.style.transform = `rotate(${360 - 360 * value}deg)`;
+  $sunIcon.style.transform = `rotate(-${360 - 360 * value}deg)`;
 });
